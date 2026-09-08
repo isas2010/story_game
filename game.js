6de82055a6f98e7d104def59402c885e8bbbd00f
Isabella Vinas <90414376+isas2010@users.noreply.github.com>
2026-09-08 16:04:21 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,35 @@
-
+<!DOCTYPE html>
+2
+<html>
+3
+<head>
+4
+<title>Story Game</title>
+5
+<link rel="stylesheetdy>
+6
+ 
+7
+<div id="gameArea">
+8
+ 
+9
+<div id="character"></div>
+10
+ 
+11
+<div id="popup" class="hidden"></div>
+12
+ 
+13
+</div>
+14
+ 
+15
+game.jsscript>
+16
+ 
+17
+</body>
+18
+</html>
