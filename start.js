9e6bfcf890ddc53af494693ff8968cb9232da3dc
Isabella Vinas <90414376+isas2010@users.noreply.github.com>
2026-09-09 09:10:37 +0200

    Update start.js

--- start.js
+++ start.js
@@ -3,8 +3,3 @@ document.getElementById("startBtn").addEventListener("click", () => {
 alert("Button works!");
 3
 });
-//document.getElementById("startBtn").addEventListener("click", () => {
-//2
-//window.location.href = "game.html";
-//3
-//});
